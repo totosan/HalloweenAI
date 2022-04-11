325724ef9c704aaca13781d9493e35286ca2d7b1
Thomas Tomow <toto_san@live.com>
2022-04-11 17:51:26 +0000

    splitted deploy from build job for video part

--- faceDetectApp/readCache.py
+++ faceDetectApp/readCache.py
@@ -1,5 +1,7 @@
+import itertools
 from os import access
 import redis
+from itertools import zip_longest
 
 r = redis.StrictRedis(host='facesink002.redis.cache.windows.net', 
                         port=6380,
@@ -11,7 +13,21 @@ r = redis.StrictRedis(host='facesink002.redis.cache.windows.net',
 #    print(key)
 #    # delete the key
 #    r.delete(key)
-if True:
+
+# iterate a list in batches of size n
+def batcher(iterable, n):
+    args = [iter(iterable)] * n
+    return zip_longest(*args)
+
+def iterateInBatches():
+    # in batches of 500 delete keys matching user:*
+    for keybatch in batcher(r.scan_iter('*'),500):
+        #r.delete(*keybatch)
+        print(keybatch)
+
+iterateInBatches()
+
+if False:
     with r.monitor() as m:
         for command in m.listen():
             print(command["command"])
