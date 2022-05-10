5c60d2eea771ab335ba218c2aae4b88e17d05a8f
Thomas Tomow <toto_san@live.com>
2022-05-10 14:30:12 +0000

    added dotnet 6 to devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,6 +2,3 @@
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes && \
 docker buildx create --name multiarch --driver docker-container --use && \
 docker buildx inspect --bootstrap
-
-alias lastFaceAppVersion=`docker images | grep -v facedetectionapp | grep amd | awk '{print $2}' | sort | tail -n1 | cut -d '-' -f2`
-alias lastFaceServerVersion=`docker images | grep facedetectionapp | grep amd | awk '{print $2}' | sort | tail -n1 | cut -d '-' -f2`
