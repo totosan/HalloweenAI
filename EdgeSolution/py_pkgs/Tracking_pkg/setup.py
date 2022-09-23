2f5320da53cb8b975d7e4686c78423e514947cac
Thomas Tomow <toto_san@live.com>
2022-09-23 12:33:05 +0000

    added apcakge data to setup.py

--- EdgeSolution/py_pkgs/Tracking_pkg/setup.py
+++ EdgeSolution/py_pkgs/Tracking_pkg/setup.py
@@ -6,5 +6,6 @@ url='www.github.com/totosan/halloweenai',
 author='Thomas Tomow',
 install_requires=['opencv-contrib-python==4.5.3.56'],
 author_email='toto_san@live.com',
+package_data={'Tracking': ['dnn/face_detector/*']},
 packages=setuptools.find_packages(),
 zip_safe=False)
